57df702d834fdc89d6fd4624cb3c907bd5ae071c
josegoval <56361627+josegoval@users.noreply.github.com>
2020-12-27 19:36:37 +0100

    Adding pug

---
+++ src/views/CreateAccount.vue
@@ -0,0 +1,11 @@
+<template> </template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from "@/components/HelloWorld.vue";
+
+export default {
+  name: "CreateAccount",
+  components: {},
+};
+</script>
