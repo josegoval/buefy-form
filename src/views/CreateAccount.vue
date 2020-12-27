219ec58820434fbbd33114d7b0d57ad541693c09
josegoval <56361627+josegoval@users.noreply.github.com>
2020-12-27 20:00:26 +0100

    Buefy working && creating form

--- src/views/CreateAccount.vue
+++ src/views/CreateAccount.vue
@@ -1,9 +1,38 @@
 <template lang="pug">
-  h1 hola
+  .container.is-fluid
+    .form
+      b-field(
+        label="FirstName"
+        :type="{'is-danger': formErrors.firstName}"
+        :message="formErrors.firstName"
+        )
+        b-input(
+          type="text"
+          v-model="form.firstName"
+          )
 </template>
 
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      form: {
+        firstName: "",
+        lastName: "",
+        username: "",
+        password: "",
+        confirmPassword: "",
+      },
+      formErrors: {
+        firstName: "",
+        lastName: "",
+        username: "",
+        password: "",
+        confirmPassword: "",
+      },
+    };
+  },
+};
 </script>
 
 <style></style>
