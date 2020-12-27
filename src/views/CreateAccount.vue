ec1898e2eadb3e61a41c9578355ddadc02cbe830
josegoval <56361627+josegoval@users.noreply.github.com>
2020-12-27 19:44:59 +0100

    Installing pug, buefy and scripts to pug Changes to be committed: 	modified:   package.json 	modified:   src/App.vue 	new file:   src/assets/scss/_variables.scss 	new file:   src/assets/scss/app.scss 	modified:   src/main.js 	modified:   src/router/index.js 	modified:   src/views/CreateAccount.vue 	modified:   yarn.lock

--- src/views/CreateAccount.vue
+++ src/views/CreateAccount.vue
@@ -1,11 +1,9 @@
-<template> </template>
+<template lang="pug">
+  h1 hola
+</template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
-
-export default {
-  name: "CreateAccount",
-  components: {},
-};
+export default {};
 </script>
+
+<style></style>
