e7272f5fffbb6df2cfaa598e79437f6a683d3559
josegoval <56361627+josegoval@users.noreply.github.com>
2020-12-28 19:17:24 +0100

    Form to component and props

---
+++ src/components/CreateUserForm.vue
@@ -0,0 +1,88 @@
+<template lang="pug">
+    .form
+      .title Create a new account
+      b-field(
+        label="First name"
+        :type="{'is-danger': hasError('firstName')}"
+        :message="getError('firstName')"
+        )
+        b-input(
+          type="text"
+          v-model="form.firstName"
+          )
+      b-field(
+        label="Last name"
+        :type="{'is-danger': hasError('lastName')}"
+        :message="getError('lastName')"
+        )
+        b-input(
+          type="text"
+          v-model="form.lastName"
+          )
+      b-field(
+        label="Username*"
+        :type="{'is-danger': hasError('username')}"
+        :message="getError('username')"
+        )
+        b-input(
+          type="text"
+          v-model="form.username"
+          )
+      b-field(
+        label="Password*"
+        :type="{'is-danger': hasError('password')}"
+        :message="getError('password')"
+        )
+        b-input(
+          type="password"
+          v-model="form.password"
+          password-reveal
+          )
+      b-field(
+        label="Confirm password*"
+        :type="{'is-danger': hasError('confirmPassword')}"
+        :message="getError('confirmPassword')"
+        )
+        b-input(
+          type="password"
+          v-model="form.confirmPassword"
+          password-reveal
+          )
+      b-button(
+        type="is-primary"
+        @click="handleSubmit"
+      ) Submit
+</template>
+
+<script>
+export default {
+  name: "CreateUserForm",
+  props: {
+    form: {
+      type: Object,
+      required: true,
+    },
+    formErrors: {
+      type: Object,
+      required: true,
+    },
+    handleSubmit: {
+      type: Function,
+      required: true,
+    },
+  },
+  methods: {
+    hasError(key) {
+      return (
+        Object.prototype.hasOwnProperty.call(this.formErrors, key) &&
+        this.formErrors[key] !== ""
+      );
+    },
+    getError(key) {
+      return this.hasError(key) ? this.formErrors[key] : "";
+    },
+  },
+};
+</script>
+
+<style></style>
--- src/views/CreateAccount.vue
+++ src/views/CreateAccount.vue
@@ -1,63 +1,16 @@
 <template lang="pug">
   .container.is-fluid
-    .form
-      .title Create a new account
-      b-field(
-        label="First name"
-        :type="{'is-danger': hasError('firstName')}"
-        :message="getError('firstName')"
-        )
-        b-input(
-          type="text"
-          v-model="form.firstName"
-          )
-      b-field(
-        label="Last name"
-        :type="{'is-danger': hasError('lastName')}"
-        :message="getError('lastName')"
-        )
-        b-input(
-          type="text"
-          v-model="form.lastName"
-          )
-      b-field(
-        label="Username*"
-        :type="{'is-danger': hasError('username')}"
-        :message="getError('username')"
-        )
-        b-input(
-          type="text"
-          v-model="form.username"
-          )
-      b-field(
-        label="Password*"
-        :type="{'is-danger': hasError('password')}"
-        :message="getError('password')"
-        )
-        b-input(
-          type="password"
-          v-model="form.password"
-          password-reveal
-          )
-      b-field(
-        label="Confirm password*"
-        :type="{'is-danger': hasError('confirmPassword')}"
-        :message="getError('confirmPassword')"
-        )
-        b-input(
-          type="password"
-          v-model="form.confirmPassword"
-          password-reveal
-          )
-      b-button(
-        type="is-primary"
-        @click="handleSubmit"
-      ) Submit
+    CreateUserForm(:form="form" :formErrors="formErrors" :handleSubmit="handleSubmit")
 </template>
 
 <script>
+import CreateUserForm from "@/components/CreateUserForm.vue";
+
 export default {
   name: "CreateAccount",
+  components: {
+    CreateUserForm,
+  },
   data() {
     return {
       form: {
@@ -115,13 +68,13 @@ export default {
     },
     ensuresValidPassword() {
       const validPassword =
-        this.form.password <= 8 || !isNaN(Number(this.form.password))
+        this.form.password.length <= 8 || !isNaN(Number(this.form.password))
           ? !this.setError(
               "password",
               "Please write more than 8 chars and not only numbers."
             )
           : !this.setError("password", "");
-      let validConfirmPassword =
+      const validConfirmPassword =
         this.form.password !== this.form.confirmPassword
           ? !this.setError(
               "confirmPassword",
@@ -137,15 +90,6 @@ export default {
     setError(key, errorMessage) {
       this.formErrors[key] = errorMessage;
       return errorMessage !== "";
-    },
-    hasError(key) {
-      return (
-        Object.prototype.hasOwnProperty.call(this.formErrors, key) &&
-        this.formErrors[key] !== ""
-      );
-    },
-    getError(key) {
-      return this.hasError(key) ? this.formErrors[key] : "";
     },
     handleSubmit() {
       const validFirstName = this.ensuresValidFirstName();
